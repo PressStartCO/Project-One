effec07ef01d47ea137ec1be9e9ec4cfb886782a
Press Start <luiz.ricardo.mtorres@gmail.com>
2018-12-07 21:09:42 -0200

    Modificações no Script
    Modifiquei o Script para reagir as animações de Andar e para virar o Sprite ao andar para esquerda.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,24 @@ public float fastmove;
 	}
 	
 	void Update () {
-		Rigidbody2D rididbody = GetComponent<Rigidbody2D>();	
+		    Rigidbody2D rididbody = GetComponent<Rigidbody2D>();	
 		//movimento_horizontal
 			float move = Input.GetAxis("Horizontal");
-			rididbody.velocity = new Vector2(move*fastmove,rididbody.velocity.y);	
+			rididbody.velocity = new Vector2(move*fastmove,rididbody.velocity.y);
+        //virar o sprite para a esqueda.
+            if (move < 0){
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if(move > 0){
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+        //Animação Walking.
+        if(move != 0) {
+            GetComponent<Animator>().SetBool("IsWalking", true);
+        }
+        else {
+            GetComponent<Animator>().SetBool("IsWalking", false);
+        }
 		//jump
 			if(Input.GetKeyDown(KeyCode.Space))
 			{rididbody.AddForce(new Vector2(0,200));}
