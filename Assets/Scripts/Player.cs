9daccbd06216eb15109253287551ad681e848318
sanysdl <sanysdl@gmail.com>
2018-12-06 23:08:09 -0200

    script test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
-	// Use this for initialization
+public float fastmove;
 	void Start () {
-		
+			
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
-}
+		Rigidbody2D rididbody = GetComponent<Rigidbody2D>();	
+		//movimento_horizontal
+			float move = Input.GetAxis("Horizontal");
+			rididbody.velocity = new Vector2(move*fastmove,rididbody.velocity.y);	
+		//jump
+			if(Input.GetKeyDown(KeyCode.Space))
+			{rididbody.AddForce(new Vector2(0,200));}
+}}
